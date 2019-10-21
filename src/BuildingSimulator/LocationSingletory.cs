0fd4049d4e8644e1b357f5b61330d02a2626e414
2paketa <manosprk@gmail.com>
2019-10-21 21:22:37 +0300

    Singletory small change

--- src/BuildingSimulator/LocationSingletory.cs
+++ src/BuildingSimulator/LocationSingletory.cs
@@ -2,15 +2,12 @@ namespace BuildingSimulator
 {
     public class LocationSingletory
     {
-        static GroundFloor gFloor;
-        static EFloor[] eFloors = new EFloor[4];
         public static ILocation GetLocation(int floorNumber)
         {
             switch (floorNumber)
             {
                 case 0:
-                    gFloor = GroundFloor.Get();
-                    return gFloor;
+                    return GroundFloor.Get();
                 default:
                     return EFloor.Get(floorNumber);
             }
